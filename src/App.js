f74dea0dedd6a85eb8f42c44fe4f01cd4ddd1f17
laylas <laylas@vt.edu>
2026-01-04 22:34:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-// Pages (you'll create these next)
 import Home from "./pages/Home.jsx";
 
 import Blog from "./pages/Blog.jsx";
 
-// Existing route you already had
-import GiftCardPage from "./components/Gift.jsx"
 
 export default function App() {
   return (
@@ -15,8 +12,6 @@ export default function App() {
       {/* Main site pages */}
       <Route path="/" element={<Home />} />
       <Route path="/blog" element={<Blog />} />
-
-      <Route path="/:slug" element={<GiftCardPage />} />
 
       <Route path="*" element={<div style={{ padding: 24 }}>Not found</div>} />
     </Routes>
